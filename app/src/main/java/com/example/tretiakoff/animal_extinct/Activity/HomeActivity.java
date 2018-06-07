9e387c8cd8e9334242567964a0c9e8ef27af93d7
Valentin CHEVREAU <valentin.chevreau@hetic.net>
2018-06-07 16:26:50 +0200

    xml: all - gradle

--- app/src/main/java/com/example/tretiakoff/animal_extinct/Activity/HomeActivity.java
+++ app/src/main/java/com/example/tretiakoff/animal_extinct/Activity/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.tretiakoff.animal_extinct.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,12 +18,18 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Intent myIntent = new Intent(HomeActivity.this, HomeActivity.class);
-        Bundle b = new Bundle();
 
-        startActivity(myIntent);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent moveToMainActivity = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(moveToMainActivity);
+                finish();
+            }
+        }, 1000);
 
     }
+
 }
 
 
