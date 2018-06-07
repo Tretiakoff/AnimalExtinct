80f2080e03ea57778bd17beaefdd0a81fc00e9ec
Valentin CHEVREAU <valentin.chevreau@hetic.net>
2018-06-07 11:28:32 +0200

    Valentin's Work: HomePage

---
+++ app/src/main/java/com/example/tretiakoff/animal_extinct/Activity/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.tretiakoff.animal_extinct.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.tretiakoff.animal_extinct.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Intent myIntent = new Intent(HomeActivity.this, HomeActivity.class);
+        Bundle b = new Bundle();
+
+        startActivity(myIntent);
+
+    }
+}
+
+
+
+
+
